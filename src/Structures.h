d1f7b2fbacab575d8af3ac1c3a73d9b97031ceb6
shaheed.abdol <shaheed.abdol@playsafesa.com>
2014-10-03 11:05:41 +0200

    Changed Structures header file.
    Added some preprocessor commands to check if we need to use SDL data
    types in the Structures.h file.

--- src/Structures.h
+++ src/Structures.h
@@ -10,8 +10,13 @@
 
 namespace spacebash_s
 {
-typedef unsigned char _uc;
+#if defined(USE_SDL)
+typedef Uint8 _uc;
+#else
+#define unsigned char _uc
+#endif
 
+#if defined(USE_SDL)
 typedef struct tagColor
 {
     #if SDL_BYTEORDER == SDL_BIG_ENDIAN
@@ -26,6 +31,14 @@ typedef struct tagColor
     unsigned char b;
     #endif
 } Color;
+#else
+	typedef struct tagColor{
+		unsigned char r;
+		unsigned char g;
+		unsigned char b;
+		unsigned char a;
+	} Color;
+#endif
 
 #if defined(USE_SDL)
 Uint32 GetCol(SDL_PixelFormat * pixel_fmt, _uc r, _uc g, _uc b)
@@ -33,18 +46,24 @@ Uint32 GetCol(SDL_PixelFormat * pixel_fmt, _uc r, _uc g, _uc b)
     Uint32 col = SDL_MapRGB(pixel_fmt, r, g, b);
     return col;
 }
+Uint32 GetCol_A(SDL_PixelFormat * pixel_fmt, _uc r, _uc g, _uc b, _uc a)
+{
+	Uint32 col = SDL_MapRGBA(pixel_fmt, r, g, b, a);
+	return col;
+}
 #else
 unsigned int GetCol(_uc r, _uc g, _uc b)
 {
-	unsigned int col = ((unsigned int)(((_uc)(r) | ((unsigned short)((_us)(g)) << 8)) | (((unsigned short)(_uc)(b)) << 16)));
+	unsigned int col = ((unsigned int)(((_uc)(r) | ((unsigned short)((_uc)(g)) << 8)) | (((unsigned short)(_uc)(b)) << 16)));
+	return col;
+}
+unsigned int GetCol_A(_uc r, _uc g, _uc b, _uc a)
+{
+	unsigned int col = ((unsigned int)(((_uc)(r) | ((unsigned short)((_uc)(g)) << 8)) | (((unsigned short)(_uc)(b)) << 16))) | (((unsigned int)(_uc)(a)) << 24);
 	return col;
 }
 #endif
-Uint32 GetCol_A(SDL_PixelFormat * pixel_fmt, _uc r, _uc g, _uc b, _uc a)
-{
-    Uint32 col = SDL_MapRGBA(pixel_fmt, r, g, b, a);
-    return col;
-}
+
 typedef struct tagPoint
 {
     float x;

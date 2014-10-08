08b277ad0d4947247af6e941cc62b075ae88e60e
shaheed.abdol <shaheed.abdol@playsafesa.com>
2014-10-08 09:55:12 +0200

    Nicely formatted, slightly neatened code.
    Formatted main file.
    Neatened code slightly.

--- main.cpp
+++ main.cpp
@@ -3,37 +3,35 @@
 #include <SDL/SDL.h>
 #include "src/SpaceBash.h"
 
-int main( int argc, char * args[] )
-{
-    SDL_Init( SDL_INIT_EVERYTHING );    //Start SDL
-    SpaceBash spaceBash;
-    SDL_Event e;
+int main(int argc, char *args[]) {
+  SDL_Init(SDL_INIT_EVERYTHING); // Start SDL
+  SpaceBash spaceBash;
+  SDL_Event e;
 
-    int direction = 0;
+  int direction = 0;
 
-    while ( SDL_WaitEvent(&e) )
-    {
-        if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_ESCAPE)
-            break;
-        if (e.type == SDL_MOUSEMOTION)
-            spaceBash.UpdateMouse(e.motion.x, e.motion.y);
-        if (e.type == SDL_MOUSEBUTTONDOWN)
-            spaceBash.Fire(e.button.x, e.button.y);
-        if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_LEFT)
-            direction = 1;
-        if (e.type == SDL_KEYUP && e.key.keysym.sym == SDLK_LEFT)
-            direction = 0;
-        if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_RIGHT)
-            direction = -1;
-        if (e.type == SDL_KEYUP && e.key.keysym.sym == SDLK_RIGHT)
-            direction = 0;
+  while (SDL_WaitEvent(&e)) {
+    if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_ESCAPE)
+      break;
+    if (e.type == SDL_MOUSEMOTION)
+      spaceBash.UpdateMouse(e.motion.x, e.motion.y);
+    if (e.type == SDL_MOUSEBUTTONDOWN)
+      spaceBash.Fire(e.button.x, e.button.y);
+    if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_LEFT)
+      direction = 1;
+    if (e.type == SDL_KEYUP && e.key.keysym.sym == SDLK_LEFT)
+      direction = 0;
+    if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_RIGHT)
+      direction = -1;
+    if (e.type == SDL_KEYUP && e.key.keysym.sym == SDLK_RIGHT)
+      direction = 0;
 
-        spaceBash.SetDirection(direction);
-    }
-    spaceBash.SetRunning(false);
-    SDL_Quit();    //Quit SDL
+    spaceBash.SetDirection(direction);
+  }
+  spaceBash.SetRunning(false);
+  SDL_Quit(); // Quit SDL
 
-    return 0;
+  return 0;
 }
 
 #else
@@ -42,7 +40,27 @@ int main( int argc, char * args[] )
 
 namespace _main {
 SpaceBash *g_spaceBash = nullptr;
-unsigned char * bits = nullptr;
+unsigned char *bits = nullptr;
+}
+
+void HandleKey(WPARAM wp) {
+  switch (wp) {
+  case VK_ESCAPE:
+    if (_main::g_spaceBash)
+      _main::g_spaceBash->SetRunning(false);
+    PostQuitMessage(0);
+    break;
+  case VK_LEFT:
+    if (_main::g_spaceBash)
+      _main::g_spaceBash->SetDirection(1);
+    break;
+  case VK_RIGHT:
+    if (_main::g_spaceBash)
+      _main::g_spaceBash->SetDirection(-1);
+    break;
+  default:
+    break;
+  }
 }
 
 long __stdcall WindowProcedure(HWND window, unsigned int msg, WPARAM wp,
@@ -54,17 +72,7 @@ long __stdcall WindowProcedure(HWND window, unsigned int msg, WPARAM wp,
       _main::g_spaceBash->SetRunning(false);
     return 0L;
   case WM_KEYDOWN:
-    if (wp == VK_ESCAPE) {
-      if (_main::g_spaceBash)
-        _main::g_spaceBash->SetRunning(false);
-      PostQuitMessage(0);
-    } else if (wp == VK_LEFT) {
-    if (_main::g_spaceBash)
-        _main::g_spaceBash->SetDirection(1);
-    } else if (wp == VK_RIGHT) {
-    if (_main::g_spaceBash)
-        _main::g_spaceBash->SetDirection(-1);
-    }
+    HandleKey(wp);
     return 0L;
   case WM_MOUSEMOVE:
     if (_main::g_spaceBash) {
@@ -72,13 +80,10 @@ long __stdcall WindowProcedure(HWND window, unsigned int msg, WPARAM wp,
     }
     return 0L;
   case WM_LBUTTONDOWN:
-      if (_main::g_spaceBash)
-        _main::g_spaceBash->Fire(LOWORD(lp), HIWORD(lp));
-  //	std::cout << "\nmouse left button down at (" << LOWORD(lp)
-  //		<< ',' << HIWORD(lp) << ")\n";
-  // fall thru
+    if (_main::g_spaceBash)
+      _main::g_spaceBash->Fire(LOWORD(lp), HIWORD(lp));
+    return 0L;
   default:
-    //	std::cout << '.';
     return DefWindowProc(window, msg, wp, lp);
   }
 }
@@ -102,43 +107,41 @@ int main(int argc, char *args[]) {
                                  640, 480, 0, 0, GetModuleHandle(0), 0);
     if (window) {
 
-		windowDC = GetWindowDC(window);
-		HDC hImgDC = CreateCompatibleDC(windowDC);
-		if (hImgDC == NULL)
-			MessageBox(NULL, L"Dc is NULL", L"ERROR!", MB_OK);
+      windowDC = GetWindowDC(window);
+      HDC hImgDC = CreateCompatibleDC(windowDC);
+      if (hImgDC == NULL)
+        MessageBox(NULL, L"Dc is NULL", L"ERROR!", MB_OK);
 
-		BITMAPINFO bf;
-		SetBkMode(hImgDC, TRANSPARENT);
-		SetTextColor(hImgDC, RGB(47, 67, 47));
-		SetStretchBltMode(hImgDC, COLORONCOLOR);
-		bf.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
-		bf.bmiHeader.biWidth = 640;
-		bf.bmiHeader.biHeight = 480;
-		bf.bmiHeader.biPlanes = 1;
-		bf.bmiHeader.biBitCount = 32;
-		bf.bmiHeader.biCompression = BI_RGB;
-		bf.bmiHeader.biSizeImage = (640 * 480 * (32 / 8));
-		bf.bmiHeader.biXPelsPerMeter = -1;
-		bf.bmiHeader.biYPelsPerMeter = -1;
-		bf.bmiHeader.biClrUsed = 0;
-		bf.bmiHeader.biClrImportant = 0;
-		bf.bmiColors[0].rgbBlue = 0;
-		bf.bmiColors[0].rgbGreen = 0;
-		bf.bmiColors[0].rgbRed = 0;
-		bf.bmiColors[0].rgbReserved = 0;
+      SetBkMode(hImgDC, TRANSPARENT);
+      SetTextColor(hImgDC, RGB(47, 67, 47));
+      SetStretchBltMode(hImgDC, COLORONCOLOR);
 
-		HBITMAP hImg = CreateDIBSection(hImgDC, &bf, DIB_RGB_COLORS, (void **)&_main::bits, NULL, 0);
-		if (hImg == NULL)
-			MessageBox(NULL, L"Image is NULL", L"ERROR!", MB_OK);
-		else if (hImg == INVALID_HANDLE_VALUE)
-			MessageBox(NULL, L"Image is invalid", L"Error!", MB_OK);
+      BITMAPINFO bf;
+      ZeroMemory(&bf, sizeof(BITMAPINFO));
 
-		unsigned char * bits_copy = _main::bits;
-		DWORD err = GetLastError();
+      bf.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+      bf.bmiHeader.biWidth = 640;
+      bf.bmiHeader.biHeight = 480;
+      bf.bmiHeader.biPlanes = 1;
+      bf.bmiHeader.biBitCount = 32;
+      bf.bmiHeader.biCompression = BI_RGB;
+      bf.bmiHeader.biSizeImage = (640 * 480 * (32 / 8));
+      bf.bmiHeader.biXPelsPerMeter = -1;
+      bf.bmiHeader.biYPelsPerMeter = -1;
 
-		SelectObject(hImgDC, hImg);
+      HBITMAP hImg = CreateDIBSection(hImgDC, &bf, DIB_RGB_COLORS,
+                                      (void **)&_main::bits, NULL, 0);
+      if (hImg == NULL)
+        MessageBox(NULL, L"Image is NULL", L"ERROR!", MB_OK);
+      else if (hImg == INVALID_HANDLE_VALUE)
+        MessageBox(NULL, L"Image is invalid", L"Error!", MB_OK);
 
-		spaceBash.SetBuffer(_main::bits, windowDC, hImgDC);
+      unsigned char *bits_copy = _main::bits;
+      DWORD err = GetLastError();
+
+      SelectObject(hImgDC, hImg);
+
+      spaceBash.SetBuffer(_main::bits, windowDC, hImgDC);
 
       ShowWindow(window, SW_SHOWDEFAULT);
       MSG msg;
@@ -147,32 +150,6 @@ int main(int argc, char *args[]) {
     }
   }
 
-  // int direction = 0;
-  /*
-  while (SDL_WaitEvent(&e))
-  {
-          if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_ESCAPE)
-                  break;
-          if (e.type == SDL_MOUSEMOTION)
-                  spaceBash.UpdateMouse(e.motion.x, e.motion.y);
-          if (e.type == SDL_MOUSEBUTTONDOWN)
-                  spaceBash.Fire(e.button.x, e.button.y);
-          if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_LEFT)
-                  direction = 1;
-          if (e.type == SDL_KEYUP && e.key.keysym.sym == SDLK_LEFT)
-                  direction = 0;
-          if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_RIGHT)
-                  direction = -1;
-          if (e.type == SDL_KEYUP && e.key.keysym.sym == SDLK_RIGHT)
-                  direction = 0;
-
-          spaceBash.SetDirection(direction);
-  }
-  */
-  // spaceBash.SetRunning(false);
-  // SDL_Quit();    //Quit SDL
-
   return 0;
 }
 #endif
-
